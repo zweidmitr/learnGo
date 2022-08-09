211dce02e3997c043226a12f7bcbf392e3ae4eaa
zweidmitr <zweidmitr@gmail.com>
2022-08-09 08:30:23 +0300

    lesson 28 (Modules)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learnGo
+Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.
+
+to be continued...
---
+++ lesson_28/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber"
+	//fiber2 "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println(fiber.New())
+	//fmt.Println(fiber2.New())
+
+	fmt.Println("Without dependencies")
+
+}
